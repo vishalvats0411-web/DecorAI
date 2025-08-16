dc30ff3ca04939a6078512049460fd7fc14bd5b8
Vishalvats0411 <vishalvats2026@gmail.com>
2025-08-16 12:52:54 +0530

    Fix numpy build error for Render

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Install numpy separately to ensure compatibility with opencv
+pip install numpy==1.23.5
+
+# Now install the rest of the packages
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
